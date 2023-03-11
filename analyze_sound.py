3e9c6eb2579c1e777ae582f020ca091355119802
benjoboy <serec.benjamin@gmail.com>
2023-03-11 16:37:08 +0100

    working version

--- analyze_sound.py
+++ analyze_sound.py
@@ -1,9 +1,32 @@
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
+import pyaudio
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 
+RATE = 44100
+RECORD_SECONDS = 3
+
+with wave.open('output.wav', 'wb') as wf:
+    p = pyaudio.PyAudio()
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+
+    print('Recording...')
+    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        wf.writeframes(stream.read(CHUNK))
+    print('Done')
+
+    stream.close()
+    p.terminate()
 
 
-wav_obj = wave.open('./pad.wav', 'rb')
+wav_obj = wave.open('./output.wav', 'rb')
 sample_freq = wav_obj.getframerate()
 
 
@@ -20,13 +43,12 @@ signal_array = np.frombuffer(signal_wave, dtype=np.int16)
 
 # l_channel = signal_array[0::2]
 
-
 times = np.linspace(0, n_samples/sample_freq, num=n_samples)
 
 plt.figure(figsize=(15, 5))
 plt.plot(times, signal_array)
-plt.title('Left Channel')
-plt.ylabel('Signal Value')
+plt.title('Izgovorjava "Olu"')
+plt.ylabel('Signal')
 plt.xlabel('Time (s)')
 plt.xlim(0, t_audio)
 plt.show()

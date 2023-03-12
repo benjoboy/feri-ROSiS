0e89fdae9ef3d83c1b96bd905fd2907f66bf5ed9
benjoboy <serec.benjamin@gmail.com>
2023-03-12 21:33:36 +0100

    added report and comments in code

--- analyze_sound.py
+++ analyze_sound.py
@@ -10,43 +10,44 @@ RATE = 44100
 RECORD_SECONDS = 3
 
 with wave.open('output.wav', 'wb') as wf:
+    #nastavimo število kanalov torej 1 za mono in 2 za stereo
     p = pyaudio.PyAudio()
     wf.setnchannels(CHANNELS)
+    #nastavimo število bitov na vzorec glede na format
     wf.setsampwidth(p.get_sample_size(FORMAT))
+    #nastavimo frekvenco vzorčenja
     wf.setframerate(RATE)
 
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
 
     print('Recording...')
     for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        #zapisemo v datoteko
         wf.writeframes(stream.read(CHUNK))
     print('Done')
 
     stream.close()
     p.terminate()
 
-
+#odpremo datoteko ki smo jo ravno posneli
 wav_obj = wave.open('./output.wav', 'rb')
-sample_freq = wav_obj.getframerate()
+
+st_vzorcev = wav_obj.getnframes()
+
+#stevilo sekund
+t_audio = st_vzorcev/RATE
+
+#preberemo signale
+signal_wave = wav_obj.readframes(st_vzorcev)
+signal = np.frombuffer(signal_wave, dtype=np.int16)
+
+#casovna os
+times = np.linspace(0, st_vzorcev/RATE, num=st_vzorcev)
 
 
-
-n_samples = wav_obj.getnframes()
-
-t_audio = n_samples/sample_freq
-
-n_channels = wav_obj.getnchannels()
-
-
-signal_wave = wav_obj.readframes(n_samples)
-signal_array = np.frombuffer(signal_wave, dtype=np.int16)
-
-# l_channel = signal_array[0::2]
-
-times = np.linspace(0, n_samples/sample_freq, num=n_samples)
-
+#izris signala
 plt.figure(figsize=(15, 5))
-plt.plot(times, signal_array)
+plt.plot(times, signal)
 plt.title('Izgovorjava "Olu"')
 plt.ylabel('Signal')
 plt.xlabel('Time (s)')
